9e6d1336f25995a36e874e03f96f93c4743d2a0b
Makiiiiii <makiko.lucky1231@gmail.com>
2024-03-18 18:09:41 +0900

    練習問題の追加

---
+++ practice/prac2.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+
+def main(url):
+    path = "/Applications/chromedriver"
+    driver = webdriver.Chrome(executable_path=path)
+    driver.get(url)
+
+    searchForm = driver.find_element(By.XPATH, "//textarea[@title='検索']")
+    searchForm.send_keys("麻婆豆腐")
+    searchButton = driver.find_element(By.XPATH, "//input[@value='Google 検索']")
+    searchButton.click()
+
+    time.sleep(3)
+
+    ## ここに処理を記述してね！
+
+    driver.quit()
+
+if __name__ == '__main__':
+    url = 'https://google.com'
+    main(url)
